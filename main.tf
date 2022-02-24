2665a78e7fe88745faa6a433a5526f74cf7afb96
Zeke Kaufman <zeke.kaufman@gmail.com>
2022-02-24 16:37:06 -0500

    Updated version.

--- main.tf
+++ main.tf
@@ -374,3 +374,35 @@ data "oci_core_images" "test_images" {
 output "app" {
   value = "http://${data.oci_core_vnic.app_vnic.public_ip_address}"
 }
+
+data "oci_database_autonomous_databases" "test_autonomous_databases" {
+  #Required
+  compartment_id = var.compartment_ocid
+
+  #Optional
+  db_workload  = "OLTP"
+  is_free_tier = "true"
+}
+
+resource "oci_database_autonomous_database" "test_autonomous_database" {
+  #Required
+  admin_password           = "Testalwaysfree1"
+  compartment_id           = var.compartment_ocid
+  cpu_core_count           = "1"
+  data_storage_size_in_tbs = "1"
+  db_name                  = "testadb5"
+
+  #Optional
+  db_workload  = "OLTP"
+  display_name = "test_autonomous_database"
+
+  freeform_tags = {
+    "Department" = "Finance"
+  }
+
+  is_auto_scaling_enabled = "false"
+  license_model           = "LICENSE_INCLUDED"
+  is_free_tier            = "true"
+}
+
+
